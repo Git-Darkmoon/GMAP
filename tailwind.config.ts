719d813ae5b45dd6227feaf4a6aac88cb636bf2e
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-12 15:01:55 -0500

    styles: Main color variable added to tailwind.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        myOrange: {
+          50: "#fff8e6",
+          100: "#ffebcc",
+          200: "#ffd699",
+          300: "#ffc266",
+          400: "#ffb733",
+          500: "#f48c2a", // Base color
+          600: "#d17a26",
+          700: "#ad661f",
+          800: "#895219",
+          900: "#733f14",
+        },
       },
     },
   },
