85cb28d44cc0a30d31657f27c825e43ca5c8358a
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-13 00:50:51 -0500

    feat: Added all the table look like.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,222 @@
 function HomePage() {
   return (
-    <main className="pt-20 pl-72">
-      <h1>Hello world</h1>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos porro
-      obcaecati, ad, possimus doloribus aspernatur fugit, error nostrum earum
-      facilis voluptatum qui maxime. Quos porro itaque, exercitationem veritatis
-      perferendis quasi labore ex, molestias et molestiae similique.
-      Dignissimos, eligendi aspernatur. Asperiores expedita distinctio corrupti
-      tempore similique illum officiis nobis tempora voluptate, cum voluptatibus
-      fugit quam veritatis laborum. Placeat corporis ipsa nostrum voluptates
-      fugit minima dicta, libero in minus quas, beatae eveniet alias. Delectus
-      cumque quas ducimus unde iste hic minus quos debitis molestias! Porro
-      voluptatum reprehenderit blanditiis voluptatibus, mollitia natus cum,
-      nobis explicabo est laudantium, qui architecto culpa. Iste, doloremque
-      laudantium!
+    <main className="pt-14 pl-64">
+      {/* Header code */}
+
+      <div className="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5">
+        <div className="w-full mb-1">
+          <div className="mb-4">
+            <nav className="flex mb-5" aria-label="Breadcrumb">
+              <ol className="inline-flex items-center space-x-1 text-sm font-medium md:space-x-2">
+                <li className="inline-flex items-center">
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-gray-700 hover:text-primary-600"
+                  >
+                    <svg
+                      className="w-5 h-5 mr-2.5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                    </svg>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <div className="flex items-center">
+                    <svg
+                      className="w-6 h-6 text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    <a
+                      href="#"
+                      className="ml-1 text-gray-700 hover:text-primary-600 md:ml-2"
+                    >
+                      E-commerce
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center">
+                    <svg
+                      className="w-6 h-6 text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    <span
+                      className="ml-1 text-gray-400 md:ml-2"
+                      aria-current="page"
+                    >
+                      Products
+                    </span>
+                  </div>
+                </li>
+              </ol>
+            </nav>
+            <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl">
+              All products
+            </h1>
+          </div>
+          <div className="items-center justify-between block sm:flex md:divide-x md:divide-gray-100">
+            <div className="flex items-center mb-4 sm:mb-0">
+              <form className="sm:pr-3" action="#" method="GET">
+                <label htmlFor="products-search" className="sr-only">
+                  Search
+                </label>
+                <div className="relative w-48 mt-1 sm:w-64 xl:w-96">
+                  <input
+                    type="text"
+                    name="email"
+                    id="products-search"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                    placeholder="Search for products"
+                  />
+                </div>
+              </form>
+              <div className="flex items-center w-full sm:justify-end">
+                <div className="flex pl-2 space-x-1">
+                  <a
+                    href="#"
+                    className="inline-flex justify-center p-1 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100"
+                  >
+                    <svg
+                      className="w-6 h-6"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                  {/* Add other buttons as needed */}
+                </div>
+              </div>
+            </div>
+            <button
+              id="createProductButton"
+              className="text-slate-800 bg-myOrange-400 hover:bg-myOrange-500 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5"
+              type="button"
+              data-drawer-target="drawer-create-product-default"
+              data-drawer-show="drawer-create-product-default"
+              aria-controls="drawer-create-product-default"
+              data-drawer-placement="right"
+            >
+              Añadir Maratonista
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Table Code */}
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th scope="col" className="p-4">
+                <div className="flex items-center">
+                  <input
+                    id="checkbox-all-search"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+                  />
+                  <label htmlFor="checkbox-all-search" className="sr-only">
+                    checkbox
+                  </label>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Product name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Color
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Accessories
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Available
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Weight
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Repeat the following block for each product */}
+            <tr className="bg-white border-b hover:bg-gray-50">
+              <td className="w-4 p-4">
+                <div className="flex items-center">
+                  <input
+                    id="checkbox-table-search-1"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+                  />
+                  <label htmlFor="checkbox-table-search-1" className="sr-only">
+                    checkbox
+                  </label>
+                </div>
+              </td>
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+              >
+                Apple MacBook Pro 17
+              </th>
+              <td className="px-6 py-4">Silver</td>
+              <td className="px-6 py-4">Laptop</td>
+              <td className="px-6 py-4">Yes</td>
+              <td className="px-6 py-4">Yes</td>
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-6 py-4">3.0 lb.</td>
+              <td className="flex items-center px-6 py-4">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:underline"
+                >
+                  Edit
+                </a>
+                <a
+                  href="#"
+                  className="font-medium text-red-600 hover:underline ms-3"
+                >
+                  Remove
+                </a>
+              </td>
+            </tr>
+            {/* Repeat the above block for each product */}
+          </tbody>
+        </table>
+      </div>
     </main>
   )
 }
