b15a600b493f24463d082dae0d6cd4accf60d1cd
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-14 23:11:38 -0500

    fix: sidebar-toggle

--- src/components/SidebarEl.tsx
+++ src/components/SidebarEl.tsx
@@ -11,17 +11,11 @@ export default function SidebarEl({ icon, text }: SidebarElType) {
     <li>
       <button
         type="button"
-        className="flex items-center w-full p-2 text-base text-slate-600 hover:bg-slate-100 hover:text-slate-900 transition duration-75 rounded-lg group "
+        className="flex items-center w-full p-2 text-base text-slate-600 hover:bg-myOrange-100 hover:text-slate-900 transition duration-75 rounded-lg group "
         aria-controls="dropdown-pages"
-        data-collapse-toggle="dropdown-pages"
       >
         {icon}
-        <span
-          className="flex-1 ml-3 text-left whitespace-nowrap"
-          sidebar-toggle-item
-        >
-          {text}
-        </span>
+        <span className="flex-1 ml-3 text-left whitespace-nowrap">{text}</span>
       </button>
     </li>
   )
