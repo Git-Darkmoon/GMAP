fba09929fa89c53957a5cb34997b43ae555c041d
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-14 19:09:11 -0500

    feat: HTTP requests for a single user.

---
+++ src/app/api/users/[student_id]/route.ts
@@ -0,0 +1,86 @@
+import { connection } from "@/utils/database"
+import { NextResponse } from "next/server"
+
+export async function GET(request: NextResponse, { params }: any) {
+  try {
+    const singleGetQuery = "SELECT * FROM maratonistas WHERE student_id = $1"
+    const singleQueryValue = [Number(params.student_id)]
+    const response = await connection.query(singleGetQuery, singleQueryValue)
+
+    if (response.rows.length === 0)
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      })
+
+    return NextResponse.json({
+      message: `User found in the Database`,
+      data: response.rows[0],
+    })
+  } catch (error) {
+    console.error(error)
+
+    return NextResponse.json({
+      message: `Error fetching user: ${params.student_id}`,
+    })
+  }
+}
+
+export async function PUT(request: NextResponse, { params }: any) {
+  const body = await request.json()
+
+  const { first_name, last_name, email, password, team, course_check } = body
+
+  try {
+    const singleGetQuery =
+      "UPDATE maratonistas SET first_name = $1, last_name = $2, email = $3, password = $4, team = $5, course_check = $6 WHERE student_id = $7 RETURNING *"
+    const studentToModify = [params.student_id]
+    const values = [
+      first_name,
+      last_name,
+      email,
+      password,
+      team,
+      course_check,
+      Number(studentToModify),
+    ]
+    const response = await connection.query(singleGetQuery, values)
+
+    if (response.rows.length === 0)
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      })
+
+    return NextResponse.json({
+      message: `User information updated in database`,
+      data: response.rows[0],
+    })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({
+      message: `Error modifying data of user: ${params.student_id}`,
+    })
+  }
+}
+
+export async function DELETE(request: NextResponse, { params }: any) {
+  try {
+    const singleGetQuery =
+      "DELETE FROM maratonistas WHERE student_id = $1 RETURNING *"
+    const singleQueryValue = [params.student_id]
+    const response = await connection.query(singleGetQuery, singleQueryValue)
+
+    if (response.rows.length === 0)
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      })
+
+    return NextResponse.json({
+      message: `User deleted successfully`,
+      data: response.rows[0],
+    })
+  } catch (error) {
+    return NextResponse.json({
+      message: `Error deleting user: ${params.student_id}`,
+    })
+  }
+}
