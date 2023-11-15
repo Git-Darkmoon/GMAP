6af911b36e23055b45936964c4c68ae6facbe92b
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-14 23:10:11 -0500

    feat: Retrieving data from the db to render table

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,32 @@
-function HomePage() {
+import HomeIcon from "@mui/icons-material/Home"
+import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded"
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined"
+import TableRowEl from "@/components/TableRowEl"
+import { Maratonista } from "@/utils/userType"
+import TableHeadEl from "@/components/TableHeadEl"
+
+async function getAllMaratonistas() {
+  const res = await fetch("http://localhost:3000/api/users")
+  const users = await res.json()
+  return users
+}
+
+async function HomePage() {
+  const maratonistas = await getAllMaratonistas()
+
+  const thTitles: string[] = [
+    "Nombre Completo",
+    "Codigo",
+    "Email",
+    "Equipo",
+    "Acceso a",
+  ]
+
   return (
-    <main className="pt-14 pl-64">
+    <main className="lg:pt-16 lg:pl-64">
       {/* Header code */}
 
-      <div className="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5">
+      <header className="p-4 pt-20 lg:pt-0 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5">
         <div className="w-full mb-1">
           <div className="mb-4">
             <nav className="flex mb-5" aria-label="Breadcrumb">
@@ -11,71 +34,31 @@ function HomePage() {
                 <li className="inline-flex items-center">
                   <a
                     href="#"
-                    className="inline-flex items-center text-gray-700 hover:text-primary-600"
+                    className="inline-flex items-center text-slate-700 hover:text-primary-600"
                   >
-                    <svg
-                      className="w-5 h-5 mr-2.5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-                    </svg>
+                    <HomeIcon />
                     Home
                   </a>
                 </li>
                 <li>
                   <div className="flex items-center">
-                    <svg
-                      className="w-6 h-6 text-gray-400"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                    <a
-                      href="#"
-                      className="ml-1 text-gray-700 hover:text-primary-600 md:ml-2"
-                    >
-                      E-commerce
-                    </a>
-                  </div>
-                </li>
-                <li>
-                  <div className="flex items-center">
-                    <svg
-                      className="w-6 h-6 text-gray-400"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
+                    <KeyboardArrowRightRoundedIcon className="text-slate-600" />
                     <span
                       className="ml-1 text-gray-400 md:ml-2"
                       aria-current="page"
                     >
-                      Products
+                      Maratonistas
                     </span>
                   </div>
                 </li>
               </ol>
             </nav>
             <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl">
-              All products
+              Todos los maratonistas
             </h1>
           </div>
           <div className="items-center justify-between block sm:flex md:divide-x md:divide-gray-100">
-            <div className="flex items-center mb-4 sm:mb-0">
+            <div className="flex items-center mb-4 sm:mb-0 gap-12 sm:gap-0">
               <form className="sm:pr-3" action="#" method="GET">
                 <label htmlFor="products-search" className="sr-only">
                   Search
@@ -85,37 +68,24 @@ function HomePage() {
                     type="text"
                     name="email"
                     id="products-search"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-                    placeholder="Search for products"
+                    className="bg-gray-50 border w-full border-gray-300 sm:text-sm rounded-lg block p-2.5 text-slate-600 focus:border-myOrange-400 focus:outline-none focus:ring-1 focus:ring-myOrange-500 focus:shadow-md"
+                    placeholder="Busca maratonistas"
+                    autoComplete="off"
                   />
                 </div>
               </form>
               <div className="flex items-center w-full sm:justify-end">
                 <div className="flex pl-2 space-x-1">
-                  <a
-                    href="#"
+                  <InfoOutlinedIcon
+                    fontSize="large"
                     className="inline-flex justify-center p-1 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100"
-                  >
-                    <svg
-                      className="w-6 h-6"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </a>
-                  {/* Add other buttons as needed */}
+                  />
                 </div>
               </div>
             </div>
             <button
               id="createProductButton"
-              className="text-slate-800 bg-myOrange-400 hover:bg-myOrange-500 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5"
+              className="text-slate-700 bg-myOrange-300 hover:bg-myOrange-400 hover:shadow-sm hover:text-slate-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 transition-all"
               type="button"
               data-drawer-target="drawer-create-product-default"
               data-drawer-show="drawer-create-product-default"
@@ -126,97 +96,28 @@ function HomePage() {
             </button>
           </div>
         </div>
-      </div>
+      </header>
 
       {/* Table Code */}
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <section className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
-              <th scope="col" className="p-4">
-                <div className="flex items-center">
-                  <input
-                    id="checkbox-all-search"
-                    type="checkbox"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-                  />
-                  <label htmlFor="checkbox-all-search" className="sr-only">
-                    checkbox
-                  </label>
-                </div>
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Color
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Accessories
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Available
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Weight
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Action
-              </th>
+              {/* 6 th */}
+              {thTitles.map((title, index) => {
+                return <TableHeadEl key={index} title={title} />
+              })}
             </tr>
           </thead>
           <tbody>
-            {/* Repeat the following block for each product */}
-            <tr className="bg-white border-b hover:bg-gray-50">
-              <td className="w-4 p-4">
-                <div className="flex items-center">
-                  <input
-                    id="checkbox-table-search-1"
-                    type="checkbox"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-                  />
-                  <label htmlFor="checkbox-table-search-1" className="sr-only">
-                    checkbox
-                  </label>
-                </div>
-              </td>
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-              >
-                Apple MacBook Pro 17
-              </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">Yes</td>
-              <td className="px-6 py-4">Yes</td>
-              <td className="px-6 py-4">$2999</td>
-              <td className="px-6 py-4">3.0 lb.</td>
-              <td className="flex items-center px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 hover:underline"
-                >
-                  Edit
-                </a>
-                <a
-                  href="#"
-                  className="font-medium text-red-600 hover:underline ms-3"
-                >
-                  Remove
-                </a>
-              </td>
-            </tr>
-            {/* Repeat the above block for each product */}
+            {maratonistas.data.map((maratonista: Maratonista) => {
+              return (
+                <TableRowEl key={maratonista.student_id} {...maratonista} />
+              )
+            })}
           </tbody>
         </table>
-      </div>
+      </section>
     </main>
   )
 }
