f9b09a5ca42c7328967545e818dbb902d3691b52
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-15 23:39:19 -0500

    feat: Login working.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,49 @@
+"use client"
+
 import Image from "next/image"
 import GitHubIcon from "@mui/icons-material/GitHub"
 import GoogleIcon from "@mui/icons-material/Google"
 import Link from "next/link"
+import { FormEvent, useState } from "react"
+import { useRouter } from "next/navigation"
+import Swal from "sweetalert2"
+
+const handleSubmit = async (
+  e: FormEvent<HTMLFormElement>,
+  ID: string | number,
+  PW: string,
+  router: any
+) => {
+  e.preventDefault()
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${ID}`)
+  const singleUser = await response.json()
+
+  if (PW === singleUser.data.password) {
+    router.push("/home")
+  } else {
+    Swal.fire({
+      title: "Datos invalidos",
+      text: "Comprueba tu información",
+      icon: "warning",
+    })
+  }
+}
 
 function LoginPage() {
+  const [ID, setID] = useState<string | number>("")
+  const [PW, setPW] = useState<string>("")
+  const router = useRouter()
+
   return (
     <div className="flex items-center min-h-screen bg-slate-50 select-none">
       <div className="flex-1 h-full max-w-4xl mx-auto bg-white rounded-lg shadow-lg lg:shadow-2xl">
-        <form className="flex flex-col md:flex-row">
+        <form
+          className="flex flex-col md:flex-row"
+          onSubmit={(e) => handleSubmit(e, ID, PW, router)}
+        >
           <div className="h-32 md:h-auto md:w-1/2">
             <Image
-              className="object-cover w-full h-full rounded-tl-lg rounded-bl-lg"
+              className="object-cover w-full h-full lg:rounded-tl-lg lg:rounded-bl-lg"
               src="/Login_image.webp"
               alt="img"
               width={500}
@@ -44,6 +77,7 @@ function LoginPage() {
                   type="number"
                   className="w-full px-4 py-2 text-sm text-slate-500 border rounded-md focus:border-myOrange-400 focus:outline-none focus:ring-1 focus:ring-myOrange-500 focus:shadow-md"
                   placeholder="20181020172"
+                  onChange={(e) => setID(e.target.value)}
                   required
                 />
               </div>
@@ -58,6 +92,7 @@ function LoginPage() {
                   id="login_password"
                   className="w-full px-4 py-2 text-sm text-slate-500 border rounded-md focus:border-myOrange-400 focus:outline-none focus:ring-1 focus:ring-myOrange-500 focus:shadow-md"
                   type="password"
+                  onChange={(e) => setPW(e.target.value)}
                   placeholder="********"
                   pattern="^(?=.*[A-Z])(?=.*\d).{8,}$"
                   title="La contraseña debe incluir una mayuscula, un numero y debe ser de 8 caracteres minimo. "
