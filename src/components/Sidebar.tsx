3e928bb46204dda5b5f07f4dfac3aea40f3a69d9
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-18 19:44:04 -0500

    feat: Added the path to each link on the sidebar.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,25 +1,26 @@
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded"
 import Groups2RoundedIcon from "@mui/icons-material/Groups2Rounded"
-import ManageAccountsRoundedIcon from "@mui/icons-material/ManageAccountsRounded"
-import GitHubIcon from "@mui/icons-material/GitHub"
+// import ManageAccountsRoundedIcon from "@mui/icons-material/ManageAccountsRounded"
 import SidebarEl, { SidebarElType } from "./SidebarEl"
 
 const sidebarElements: SidebarElType[] = [
   {
     id: 1,
     icon: <HomeRoundedIcon />,
-    text: "Home",
+    text: "Noticias",
+    path: "/home/news",
   },
   {
     id: 2,
     icon: <Groups2RoundedIcon />,
     text: "Maratonistas",
+    path: "/home/maratonistas",
   },
-  {
-    id: 3,
-    icon: <ManageAccountsRoundedIcon />,
-    text: "Perfil",
-  },
+  // {
+  //   id: 3,
+  //   icon: <ManageAccountsRoundedIcon />,
+  //   text: "Perfil",
+  // },
 ]
 
 function Sidebar() {
@@ -37,16 +38,6 @@ function Sidebar() {
                 return <SidebarEl key={el.id} {...el} />
               })}
             </ul>
-            <div className="pt-2 space-y-2">
-              <a
-                href="https://github.com/Git-Darkmoon/GMAP.git"
-                target="_blank"
-                className="flex items-center p-2 text-base text-slate-600 transition duration-75 rounded-lg hover:text-slate-900 hover:bg-slate-100 group"
-              >
-                <GitHubIcon />
-                <span className="ml-3">GitHub Repository</span>
-              </a>
-            </div>
           </div>
         </div>
       </div>
