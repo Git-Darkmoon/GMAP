83768de1ee4666ae3d971818b14913c7ec9ec3d9
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-18 19:42:20 -0500

    feat: component update to <Link/>

--- src/components/SidebarEl.tsx
+++ src/components/SidebarEl.tsx
@@ -1,22 +1,23 @@
+import Link from "next/link"
 import { ReactElement } from "react"
 
 export type SidebarElType = {
   id?: number
   icon: ReactElement
   text: string
+  path: string
 }
 
-export default function SidebarEl({ icon, text }: SidebarElType) {
+export default function SidebarEl({ icon, text, path }: SidebarElType) {
   return (
     <li>
-      <button
-        type="button"
+      <Link
+        href={path}
         className="flex items-center w-full p-2 text-base text-slate-600 hover:bg-myOrange-100 hover:text-slate-900 transition duration-75 rounded-lg group "
-        aria-controls="dropdown-pages"
       >
         {icon}
         <span className="flex-1 ml-3 text-left whitespace-nowrap">{text}</span>
-      </button>
+      </Link>
     </li>
   )
 }
