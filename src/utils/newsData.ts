26e2e5100039730d3c02914aa4558c64caedcb20
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-18 19:40:23 -0500

    feat: data for the programming news.

---
+++ src/utils/newsData.ts
@@ -0,0 +1,39 @@
+import { News } from "./myTypes"
+
+export const newsData: News[] = [
+  {
+    image: "/newsImages/maraton_complexity.webp",
+    title: "Complexity",
+    desc: "...",
+  },
+  {
+    image: "/newsImages/maraton_cybersecurity.webp",
+    title: "Complexity",
+    desc: "...",
+  },
+  {
+    image: "/newsImages/maraton_dataAnalyst.webp",
+    title: "Complexity",
+    desc: "...",
+  },
+  {
+    image: "/newsImages/maraton_finantialPrediction.webp",
+    title: "Complexity",
+    desc: "...",
+  },
+  {
+    image: "/newsImages/maraton_IA.webp",
+    title: "Complexity",
+    desc: "...",
+  },
+  {
+    image: "/newsImages/maraton_progAvanzada.webp",
+    title: "Complexity",
+    desc: "...",
+  },
+  {
+    image: "/newsImages/maraton_progBasica.webp",
+    title: "Complexity",
+    desc: "...",
+  },
+]
