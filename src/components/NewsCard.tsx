dd2b8cdc01da2e13c9537decd2310d9f7c685452
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-18 22:56:13 -0500

    feat: Show more button functionality.

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,20 +1,34 @@
+"use client"
+
 import { News } from "@/utils/myTypes"
 import Image from "next/image"
+import { useState } from "react"
 
 const NewsCard: React.FC<News> = ({ image, title, desc }) => {
+  const [showMore, setShowMore] = useState<boolean>(false)
+
   return (
-    <div className="bg-slate-900 text-slate-100 rounded-lg shadow-md hover:shadow-lg">
+    <div className="bg-myOrange-50 self-stretch text-slate-800 rounded-lg shadow-md hover:shadow-lg hover:scale-105 transition-all">
       <Image
         src={image}
         alt={title}
         width={300}
         height={300}
-        className="w-full h-auto rounded-t-lg"
+        className="w-full rounded-t-lg"
         priority
       />
       <div className="p-4">
         <h3 className="text-xl font-bold">{title}</h3>
-        <p className="mt-2 text-gray-300">{desc}</p>
+        <p className="mt-2 text-slate-600 ">
+          {showMore ? desc : `${desc.slice(0, 130)}...`}
+
+          <button
+            className="ml-1 text-myOrange-500 py-1 hover:underline hover:underline-offset-2"
+            onClick={() => setShowMore(!showMore)}
+          >
+            {showMore ? "Show Less" : "Show More"}
+          </button>
+        </p>
       </div>
     </div>
   )
